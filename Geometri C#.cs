4d37f316511d33916a4889793f2d765f5042f123
JacobLykkeby <88821493+JacobLykkeby@users.noreply.github.com>
2021-09-01 11:52:53 +0200

    Add files via upload

---
+++ Geometri C#.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Geometri_CSharp
+{
+    public class Square
+    {
+        private int side;
+        public int Side
+        {
+            get
+            {
+                return side;
+            }
+        }
+        
+        public Square()
+        {
+            this.side = side;
+        }
+
+        static void SquareMethod()
+        {
+            return side * 4;
+
+        }
+
+        static void AreaMethod()
+        {
+            return side * side;
+        }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Square Side = new Square();
+                Console.WriteLine($"SquareMethod: {Side.SquareMethod()}" + $"AreaMethod: {Side.AreaMethod()}");
+            
+      
+        }
+
+
+
+    }
+
+
+}
