f21eba635110723c0807560f8dd242357f4dae7e
JacobLykkeby <88821493+JacobLykkeby@users.noreply.github.com>
2021-09-12 23:13:27 +0200

    Add files via upload

---
+++ En tur på bibloteket.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace En_tur_på_Bibloteket
+{
+
+    class BookList
+    {
+
+        public string Name;
+        public double Number_Of_Pages;
+        public string Author_Name;
+
+        public string name
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+        public double number_of_pages
+        {
+            get { return Number_Of_Pages; }
+            set { Number_Of_Pages = value; }
+        }
+
+        public string author_name
+        {
+            get { return Author_Name; }
+            set { Author_Name = value; }
+        }
+
+
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BookList book1 = new BookList();
+            {
+                book1.name = "Harry Potter";
+                book1.number_of_pages = 377;
+                book1.author_name = "J.K Rowling";
+            }
+
+            BookList book2 = new BookList();
+            {
+                book2.name = "Lord of the rings";
+                book2.number_of_pages = 568;
+                book2.author_name = "J.R.R. Tolkien";
+            }
+
+            BookList book3 = new BookList();
+            {
+                book3.name = "Alice's Adventures in Wonderland";
+                book3.number_of_pages = 241;
+                book3.author_name = "Lewis Carroll";
+            }
+
+        }
+    }
+}
---
+++ Jedi Mind Tricks.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Jedi_Mind_Tricks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> Maincharacter = new Dictionary<string, int>();
+            Maincharacter.Add("Belmont", 30);
+            Maincharacter.Add("Alucard", 102);
+            Maincharacter.Add("Isacc", 38);
+            Maincharacter.Add("Hector", 34);
+
+            Maincharacter["Dracula"] = 500;
+            Maincharacter["Dracula's wife"] = 50;
+
+            var Belmont = Maincharacter.First();
+            Console.WriteLine(Belmont);
+
+            var Alucard = Maincharacter.First();
+            Console.WriteLine(Alucard);
+
+            var Isacc = Maincharacter.First();
+            Console.WriteLine(Isacc);
+
+            var Hector = Maincharacter.First();
+            Console.WriteLine(Hector);
+
+            Maincharacter.Remove("Dracula's wife");
+
+            foreach (KeyValuePair<string, int> entry in Maincharacter)
+            {
+                Console.WriteLine("Key = {0}, Value = {1}",
+                    entry.Key, entry.Value);
+            }
+        }
+    }
+}
